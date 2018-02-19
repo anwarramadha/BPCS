bb6a31821fdc1223d510521808ae7f2fa9f3d891
drestanto <dyas@live.com>
2018-02-19 15:50:18 +0700

    PNSR from rms

--- coba.py
+++ coba.py
@@ -5,5 +5,18 @@ class Nyoba :
 	def __init__(self):
 		pass
 
-	def getPNSR(double rms):
-		return 10*math.log(256/rms,10)
+	def getPNSR(rms):
+		if (rms!=0):
+			return 20*math.log(256/rms,10)
+		else:
+			return 9999999999999999999999999999999999999999999999
+
+	def printPNSR(rms):
+		pnsr = Nyoba.getPNSR(rms)
+		if (pnsr==9999999999999999999999999999999999999999999999):
+			print("Tidak ada perbedaan image")
+		else:
+			print("Image berbeda dengan PNSR = ", end="")
+			print(pnsr)
+
+Nyoba.printPNSR(0.00000001)
