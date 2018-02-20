abb7e7fa2cf6398a4bfb6bc658b176470ac6ef7d
drestanto <dyas@live.com>
2018-02-20 08:31:44 +0700

    psnr tinggal itung MSE

--- coba.py
+++ coba.py
@@ -7,6 +7,8 @@ import time
 # print gambar.size #Get the width and hight of the image for iterating over
 # print pixel[0,0] #Get the RGBA Value of the a pixel of an image
 
+global max_diff
+max_diff = 255
 
 class ImageComparer :
 	def __init__(self, image1_name, image2_name):
@@ -29,6 +31,41 @@ class ImageComparer :
 		else:
 			return False
 
+	def getMSE(self):
+		if self.isImgSameSize() and self.isImgSameColorType():
+			# proses
+			return 0
+		else:
+			# image is very different
+			return max_diff
+
+# 	# img1 and img2 have to be matrix and must have the same the exact same size
+# 	def getMSE(img1, img2):
+# 		row = len(img1)
+# 		column = len(img1[0])
+# 		size = row*column
+# 		totalSelisihKuadrat = 0.0
+# 		for i in range(row):
+# 			for j in range(column):
+# 				selisih = img1[i][j]-img2[i][j]
+# 				totalSelisihKuadrat += math.pow(selisih,2)
+# 		return math.pow(totalSelisihKuadrat/size,0.5)
+
+	def getPSNR(self):
+		mse = self.getMSE
+		if (mse!=0):
+			return 20*math.log(max_diff/rms,10)
+		else:
+			return 9999999999999999999999999999999999999999999999
+
+
+	def printPSNR(self):
+		psnr = self.getPSNR()
+		if (psnr==9999999999999999999999999999999999999999999999):
+			print("Tidak ada perbedaan image")
+		else:
+			print("Image berbeda dengan PSNR = ".format(psnr))
+
 if __name__ == "__main__":
 	# INPUT
 	filename1 = raw_input("Image 1 name: ")
@@ -44,46 +81,3 @@ if __name__ == "__main__":
 	# OUTPUT
 	print("Run time")
 	print("--- %s seconds ---" % (time.time() - start_time))
-
-# class Nyoba :
-	
-# 	def __init__(self):
-# 		pass
-
-# 	# img1 or img2 must be a matrix
-# 	def isImgSameSize(img1, img2):
-# 		if (len(img1)==len(img2)) and (len(img1[0])==len(img2[0])):
-# 			return True
-# 		else:
-# 			return False
-
-# 	# img1 and img2 have to be matrix and must have the same the exact same size
-# 	def getMSE(img1, img2):
-# 		row = len(img1)
-# 		column = len(img1[0])
-# 		size = row*column
-# 		totalSelisihKuadrat = 0.0
-# 		for i in range(row):
-# 			for j in range(column):
-# 				selisih = img1[i][j]-img2[i][j]
-# 				totalSelisihKuadrat += math.pow(selisih,2)
-# 		return math.pow(totalSelisihKuadrat/size,0.5)
-
-
-# 	def getPSNR(rms, max_diff):
-# 		if (rms!=0):
-# 			return 20*math.log(max_diff/rms,10)
-# 		else:
-# 			return 9999999999999999999999999999999999999999999999
-
-# 	def printPSNR(rms, max_diff):
-# 		psnr = Nyoba.getPSNR(rms, max_diff)
-# 		if (psnr==9999999999999999999999999999999999999999999999):
-# 			print("Tidak ada perbedaan image")
-# 		else:
-# 			print("Image berbeda dengan PSNR = ".format(psnr))
-			
-
-# Nyoba.printPSNR(0.00000001, 256)
-# print(Nyoba.isImgSameSize([[1,0],[0,1]],[[2,3],[5,7]]))
-# print(Nyoba.getMSE([[1,0],[0,1]],[[1,0],[0,5]]))
